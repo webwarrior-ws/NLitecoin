318829d480440203e47a072f137e848a47881e0a
webwarrior <reg@webwarrior.ws>
2023-08-03 13:29:48 +0200

    WIP: add MimbleWimble types
    Add types needed for MimbleWimble transactions.

---
+++ src/NLitecoin/MimbleWimble/Types.fs
@@ -0,0 +1,136 @@
+﻿namespace NLitecoin.MimbleWimble
+
+open NBitcoin
+
+
+type BlindingFactor = BlindindgFactor of uint256
+
+type Hash = Hash of uint256
+
+// 33 bytes
+type PedersenCommitment = PedersenCommitment of bigint
+
+// 33 bytes
+type PublicKey = PublicKey of bigint
+
+// 64 bytes
+type Signature = Signature of bigint
+
+type InputFeatures =
+    | STEALTH_KEY_FEATURE_BIT = 0x01
+    | EXTRA_DATA_FEATURE_BIT = 0x02
+
+type OutputFeatures =
+    | STANDARD_FIELDS_FEATURE_BIT = 0x01
+    | EXTRA_DATA_FEATURE_BIT = 0x02
+
+type Input =
+    {
+        Features: InputFeatures
+        OutputID: Hash
+        Commitment: PedersenCommitment
+        InputPubKey: Option<PublicKey>
+        OutputPublicKey: PublicKey
+        ExtraData: array<uint8>
+        Signature: Signature
+    }
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
+
+type OutputMessage =
+    {
+        Features: OutputFeatures
+        KeyExchangePubkey: PublicKey
+        ViewTag: uint8
+        MaskedValue: uint64
+        // 16 bytes
+        MaskedNonce: bigint
+        ExtraData: array<uint8>
+    }
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
+
+type RangeProof(bytes: array<uint8>) =
+    do assert(bytes.Length <= 675)
+
+    member _.Data = bytes
+
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
+
+type Output =
+    {
+        Commitment: PedersenCommitment
+        SenderPublicKey: PublicKey
+        ReceiverPublicKey: PublicKey
+        Message: OutputMessage
+        RangeProof: RangeProof
+        Signature: Signature
+    }
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
+
+type KernelFeatures =
+    | FEE_FEATURE_BIT = 0x01
+    | PEGIN_FEATURE_BIT = 0x02
+    | PEGOUT_FEATURE_BIT = 0x04
+    | HEIGHT_LOCK_FEATURE_BIT = 0x08
+    | STEALTH_EXCESS_FEATURE_BIT = 0x10
+    | EXTRA_DATA_FEATURE_BIT = 0x20
+
+module KernelFeatures =
+    let ALL_FEATURE_BITS = 
+        KernelFeatures.FEE_FEATURE_BIT |||
+        KernelFeatures.PEGIN_FEATURE_BIT ||| 
+        KernelFeatures.PEGOUT_FEATURE_BIT ||| 
+        KernelFeatures.HEIGHT_LOCK_FEATURE_BIT ||| 
+        KernelFeatures.STEALTH_EXCESS_FEATURE_BIT ||| 
+        KernelFeatures.EXTRA_DATA_FEATURE_BIT
+
+type CAmount = int64
+
+type PegOutCoin =
+    {
+        Amount: CAmount
+        ScriptPubKey: NBitcoin.Script // ?
+    }
+
+type Kernel =
+    {
+        Features: KernelFeatures
+        Fee: Option<CAmount>
+        Pegin: Option<CAmount>
+        Pegouts: array<PegOutCoin>
+        LockHeight: Option<int32>
+        StealthExcess: Option<PublicKey>
+        ExtraData: array<uint8>
+        // Remainder of the sum of all transaction commitments. 
+        // If the transaction is well formed, amounts components should sum to zero and the excess is hence a valid public key.
+        Excess: PedersenCommitment
+        // The signature proving the excess is a valid public key, which signs the transaction fee.
+        Signature: Signature
+    }
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
+
+/// TRANSACTION BODY - Container for all inputs, outputs, and kernels in a transaction or block.
+type TxBody =
+    {
+        /// List of inputs spent by the transaction.
+        Inputs: array<Input>
+        /// List of outputs the transaction produces.
+        Outputs: array<Output>
+        /// List of kernels that make up this transaction.
+        Kernels: array<Kernel>
+    }
+
+type Transaction =
+    {
+        // The kernel "offset" k2 excess is k1G after splitting the key k = k1 + k2.
+        KernelOffset: BlindingFactor
+        StealthOffset: BlindingFactor
+        // The transaction body.
+        Body: TxBody
+    }
+    member self.Hash: Hash =
+        raise <| System.NotImplementedException()
