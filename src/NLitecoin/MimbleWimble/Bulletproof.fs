5e75facef3eb315185194a7837d25440f731af6e
webwarrior <reg@webwarrior.ws>
2023-08-17 13:59:49 +0200

    WIP: continue implementing range proofs

--- src/NLitecoin/MimbleWimble/Bulletproof.fs
+++ src/NLitecoin/MimbleWimble/Bulletproof.fs
@@ -1,17 +1,150 @@
 ﻿module NLitecoin.MimbleWimble.Bulletproof
 
+open System
+
 open Org.BouncyCastle.Crypto.Digests
 open Org.BouncyCastle.Crypto.Engines
 open Org.BouncyCastle.Security
 open Org.BouncyCastle.Asn1.X9
 open Org.BouncyCastle.Math
+open Org.BouncyCastle.Math.EC
 open NBitcoin
 
+let ScalarCheckOverflow (a: array<uint64>) : bool =
+    let SECP256K1_N_0 = 0xBFD25E8CD0364141UL
+    let SECP256K1_N_1 = 0xBAAEDCE6AF48A03BUL
+    let SECP256K1_N_2 = 0xFFFFFFFFFFFFFFFEUL
+    let SECP256K1_N_3 = 0xFFFFFFFFFFFFFFFFUL
+
+    let mutable yes = false
+    let mutable no = false
+    no <- (a.[3] < SECP256K1_N_3) // No need for a > check.
+    no <- no || (a.[2] < SECP256K1_N_2)
+    yes <- yes || (a.[2] > SECP256K1_N_2) && not no
+    no <- no || (a.[1] < SECP256K1_N_1)
+    yes <- yes || (a.[1] > SECP256K1_N_1) && not no
+    yes <- yes || (a.[0] >= SECP256K1_N_0) && not no
+    yes
+
+// port of https://github.com/litecoin-project/litecoin/blob/5ac781487cc9589131437b23c69829f04002b97e/src/secp256k1-zkp/src/scalar.h#L114
+let ScalarChaCha20 (seed: uint256) (index: uint64) : ECFieldElement * ECFieldElement =
+    let mutable overCount = 0
+    let seed32 = seed.ToBytes() |> Array.chunkBySize 4 |> Array.map BitConverter.ToUInt32
+    let mutable x = Array.zeroCreate<uint32> 16
+    let mutable r1 = Array.empty
+    let mutable r2 = Array.empty
+    let mutable overCount = 0u
+    let mutable over1 = true
+    let mutable over2 = true
+
+    let inline LE32 p =
+        if BitConverter.IsLittleEndian then
+            p
+        else
+            ((p &&& 0xFFu) <<< 24) ||| ((p &&& 0xFF00u) <<< 8) ||| (((p) &&& 0xFF0000u) >>> 8) ||| (((p) &&& 0xFF000000u) >>> 24)
+
+    let inline BE32 p =
+        if BitConverter.IsLittleEndian then
+            ((p &&& 0xFFUL) <<< 24) ||| ((p &&& 0xFF00UL) <<< 8) ||| ((p &&& 0xFF0000UL) >>> 8) ||| ((p &&& 0xFF000000UL) >>> 24)
+        else
+            p
+
+    let ROTL32(x, n) = ((x) <<< (n) ||| (x) >>> (32-(n)))
+
+    let QUARTERROUND (a,b,c,d) = 
+        x.[a] <- x.[a] + x.[b]
+        x.[d] <- ROTL32(x.[d] ^^^ x.[a], 16)
+        x.[c] <- x.[c] + x.[d]
+        x.[b] <- ROTL32(x.[b] ^^^ x.[c], 12)
+        x.[a] <- x.[a] + x.[b]
+        x.[d] <- ROTL32(x.[d] ^^^ x.[a], 8)
+        x.[c] <- x.[d]
+        x.[b] <- ROTL32(x.[b] ^^^ x.[c], 7)
+
+    while (over1 || over2) do
+        x <- [|
+            0x61707865u
+            0x3320646eu
+            0x79622d32u
+            0x6b206574u
+            LE32(seed32.[0])
+            LE32(seed32.[1])
+            LE32(seed32.[2])
+            LE32(seed32.[3])
+            LE32(seed32.[4])
+            LE32(seed32.[5])
+            LE32(seed32.[6])
+            LE32(seed32.[7])
+            uint32 index
+            uint32(index >>> 32)
+            0u
+            overCount
+        |]
+
+        for i=1 to 10 do
+            QUARTERROUND(0, 4, 8,12)
+            QUARTERROUND(1, 5, 9,13)
+            QUARTERROUND(2, 6,10,14)
+            QUARTERROUND(3, 7,11,15)
+            QUARTERROUND(0, 5,10,15)
+            QUARTERROUND(1, 6,11,12)
+            QUARTERROUND(2, 7, 8,13)
+            QUARTERROUND(3, 4, 9,14)
+
+        x <- 
+            Array.map2
+                (+)
+                x
+                [|
+                    0x61707865u
+                    0x3320646eu
+                    0x79622d32u
+                    0x6b206574u
+                    LE32(seed32[0]);
+                    LE32(seed32[1]);
+                    LE32(seed32[2]);
+                    LE32(seed32[3]);
+                    LE32(seed32[4]);
+                    LE32(seed32[5]);
+                    LE32(seed32[6]);
+                    LE32(seed32[7]);
+                    uint32 index
+                    uint32(index >>> 32)
+                    0u
+                    overCount
+                |]
+        r1 <-
+            [| 
+                BE32(uint64 x.[6]) <<< 32 ||| BE32(uint64 x.[7])
+                BE32(uint64 x.[4]) <<< 32 ||| BE32(uint64 x.[5])
+                BE32(uint64 x.[2]) <<< 32 ||| BE32(uint64 x.[3])
+                BE32(uint64 x.[0]) <<< 32 ||| BE32(uint64 x.[1])
+            |]
+        r2 <-
+            [|
+                BE32(uint64 x.[14]) <<< 32 ||| BE32(uint64 x.[15])
+                BE32(uint64 x.[12]) <<< 32 ||| BE32(uint64 x.[13])
+                BE32(uint64 x.[10]) <<< 32 ||| BE32(uint64 x.[11])
+                BE32(uint64 x.[8]) <<< 32 ||| BE32(uint64 x.[9])
+            |]
+
+        // maybe ECCurve.IsValidFieldElement will do the job?
+        over1 <- ScalarCheckOverflow r1
+        over2 <- ScalarCheckOverflow r2
+
+        overCount <- overCount + 1u
+
+    let createFieldElement (arr: array<uint64>) =
+        BigInteger(arr |> Array.map BitConverter.GetBytes |> Array.concat)
+        |> Pedersen.curve.Curve.FromBigInteger
+
+    createFieldElement r1, createFieldElement r2
+
 // should be equivalent to https://github.com/litecoin-project/litecoin/blob/master/src/secp256k1-zkp/src/field_impl.h#L290
-let IsQuadVar (elem: EC.ECFieldElement) =
+let IsQuadVar (elem: ECFieldElement) =
     elem.Sqrt().Square() = elem
 
-let UpdateCommit (commit: uint256) (lpt: EC.ECPoint) (rpt: EC.ECPoint) : uint256 =
+let UpdateCommit (commit: uint256) (lpt: ECPoint) (rpt: ECPoint) : uint256 =
     let lparity = 
         (if IsQuadVar lpt.AffineYCoord then 0uy else 2uy) 
         + (if IsQuadVar rpt.AffineYCoord then 0uy else 1uy)
@@ -41,13 +174,27 @@ let ConstructRangeProof (amount: uint64) (key: uint256) (privateNonce: uint256) 
         hasher.DoFinal(result, 0) |> ignore
         result
 
-    // following values are meant to be random in the Bulletproof paper, but in secp256k1-zkp
-    // they are generated from privateNonce and rewindNonce and alpha is meant to be recoverable
-    // (see https://github.com/litecoin-project/litecoin/blob/5ac781487cc9589131437b23c69829f04002b97e/src/secp256k1-zkp/src/modules/bulletproofs/rangeproof_impl.h#L526)
     let random = SecureRandom()
-    let alpha = Pedersen.curve.Curve.RandomFieldElement random
-    let rho = Pedersen.curve.Curve.RandomFieldElement random
-    let tau1 = Pedersen.curve.Curve.RandomFieldElement random
-    let tau2 = Pedersen.curve.Curve.RandomFieldElement random
+    let alpha, rho = ScalarChaCha20 rewindNonce 0UL
+    let tau1, tau2 = ScalarChaCha20 privateNonce 1UL
+
+    // Encrypt value into alpha, so it will be recoverable from -mu by someone who knows rewindNonce
+    let alpha = 
+        let vals = BigInteger.ValueOf(int64 amount) |> Pedersen.curve.Curve.FromBigInteger
+        // Combine value with 20 bytes of optional message
+        let vals_bytes = vals.GetEncoded()
+        for i=0 to 20-1 do
+            vals_bytes.[i+4] <- proofMessage.[i]
+        let vals = BigInteger vals_bytes |> Pedersen.curve.Curve.FromBigInteger
+        // Negate so it'll be positive in -mu
+        let vals = vals.Negate()
+        alpha.Add vals
+
+    let nbits = 64
+
+    // Compute A and S
+    let aL = Array.init nbits (fun i -> amount &&& uint64(1UL <<< i))
+    let aR = aL |> Array.map (fun n -> 1UL - n)
+        
     
     failwith "not implemented"
