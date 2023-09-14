9870ba17f2ccc6e516a6d3a7f2cfccba5257a402
webwarrior <reg@webwarrior.ws>
2023-09-14 10:16:57 +0200

    MimbleWimble.Tests: fix in TestBlindSwitch
    Add precondition to TestBlindSwitch that checks that blind is
    not 0.

--- tests/NLitecoin.MimbleWimble.Tests/ZKPTests.fs
+++ tests/NLitecoin.MimbleWimble.Tests/ZKPTests.fs
@@ -75,10 +75,11 @@ let TestPedersenCommit (value: uint64) (blind: BlindingFactor) =
 
 [<Property(Arbitrary=[|typeof<ByteArray32Generators>|])>]
 let TestBlindSwitch (value: uint64) (blind: BlindingFactor) =
-    use pedersen = new Secp256k1ZKP.Net.Pedersen()
-    let referenceBlind = pedersen.BlindSwitch(value, blind.ToUInt256().ToBytes())
-    let ourBlind = Pedersen.BlindSwitch blind (int64 value)
-    ourBlind = (BlindingFactor(uint256 referenceBlind))
+    blind.ToUInt256() <> uint256.Zero ==> fun () ->
+        use pedersen = new Secp256k1ZKP.Net.Pedersen()
+        let referenceBlind = pedersen.BlindSwitch(value, blind.ToUInt256().ToBytes())
+        let ourBlind = Pedersen.BlindSwitch blind (int64 value)
+        ourBlind = (BlindingFactor(uint256 referenceBlind))
 
 [<Property(Arbitrary=[|typeof<ByteArray32Generators>|])>]
 let TestBlindingFactor (factor: BlindingFactor) =
